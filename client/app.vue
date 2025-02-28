c0c16f9e1b4f05fe1a381191366f6e2dff6091d4
maktezi <mlcnierras@gmail.com>
2025-02-28 18:43:31 +0800

    feat: add client, code style and prettier

---
+++ client/app.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="text-2xl font-bold text-red-500:">Hello world!</h1>
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+    colorMode: {
+        classPrefix: "",
+        classSuffix: "-mode",
+        componentName: "ColorScheme",
+        fallback: "light",
+        globalName: "__NUXT_COLOR_MODE__",
+        hid: "nuxt-color-mode-script",
+        preference: "system",
+        storage: "localStorage",
+        storageKey: "nuxt-color-mode",
+    },
+    compatibilityDate: "2024-11-01",
+    css: ["~/assets/css/main.css"],
+    devtools: { enabled: true },
+    imports: { dirs: ["~/stores", "~/utils", "~/composables/*/*.{ts,js}"] },
+    modules: ["@nuxtjs/color-mode", "@nuxt/eslint", "@nuxt/ui"],
+    srcDir: "client/",
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
