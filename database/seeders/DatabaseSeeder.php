1770de15899752f3fedd9fab76f0e91fc933bf4b
Joey A. Cabelin <joey.cabelin.1@gmail.com>
2025-04-28 19:27:04 +0800

    feat: implement graphql-permission-crud (#51)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,22 +14,30 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        if (!Role::where('name', 'Admin')->exists()) {
-            Role::create(['name' => 'Admin']);
-        }
-        if (!Role::where('name', 'Manager')->exists()) {
-            Role::create(['name' => 'Manager']);
-        }
-        if (!Role::where('name', 'User')->exists()) {
-            Role::create(['name' => 'User']);
-        }
+    {   
         if (!Permission::where('name', 'edit user')->exists()) {
-            Permission::create(['name' => 'edit user']);
+            $editUserPermission = Permission::create(['name' => 'edit user']);
         }
         if (!Permission::where('name', 'delete user')->exists()) {
-            Permission::create(['name' => 'delete user']);
+            $deleteUserPermission = Permission::create(['name' => 'delete user']);
         }
+        if (!Permission::where('name', 'edit product')->exists()) {
+            $editProductPermission = Permission::create(['name' => 'edit product']);
+        }
+
+        if (!Role::where('name', 'Admin')->exists()) {
+            $adminRole = Role::create(['name' => 'Admin']);
+            $adminRole->givePermissionTo($editUserPermission);
+            $adminRole->givePermissionTo($deleteUserPermission);
+        }
+        if (!Role::where('name', 'Manager')->exists()) {
+            $managerRole = Role::create(['name' => 'Manager']);
+            $managerRole->givePermissionTo($editProductPermission);
+        }
+        if (!Role::where('name', 'User')->exists()) {
+            $adminRole = Role::create(['name' => 'User']);
+        }
+        
 
         $admin = User::factory()->create([
             'name' => 'Admin',
@@ -48,8 +56,7 @@ class DatabaseSeeder extends Seeder
         $users = User::factory(100)->create();
 
         $admin->assignRole('Admin');
-        $admin->givePermissionTo('edit user');
-        $admin->givePermissionTo('delete user');
+
         $manager->assignRole('Manager');
         $users->each(function ($user) {
             $user->assignRole('User');
