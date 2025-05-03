f7de3ff173a678df6535eac7e00c28f26fa981b6
maktezi <mlcnierras@gmail.com>
2025-05-03 19:35:11 +0800

    refactor: permission store

--- client/composables/useCan.ts
+++
@@ -1,6 +0,0 @@
-import { useAuthStore } from '@/stores/auth'
-
-export const useCan = (permission: string) => {
-  const authStore = useAuthStore()
-  return computed(() => authStore.can(permission))
-}
--- client/composables/useRole.ts
+++
@@ -1,6 +0,0 @@
-import { useAuthStore } from '@/stores/auth'
-
-export const useRole = (role: string) => {
-  const authStore = useAuthStore()
-  return computed(() => authStore.is(role))
-}
