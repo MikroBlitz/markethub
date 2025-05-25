5b9dec912ad242a21618fa452b50936f9e8ad938
maktezi <mlcnierras@gmail.com>
2025-05-25 21:11:16 +0800

    refactor: seeder, permissions and pages

--- client/middleware/auth.global.ts
+++ client/middleware/auth.global.ts
@@ -1,22 +1,32 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
-    // const authStore = useAuthStore();
-    // const noAuthRoutes = ["/", "/register"];
-    // const router = useRouter();
-    // const validRoutes = router
-    //     .getRoutes()
-    //     .map((route) => route.path.toLowerCase());
-    //
-    // const toPath = to.path.toLowerCase();
-    //
-    // if (!validRoutes.includes(toPath)) return navigateTo("/dashboard");
-    //
-    // if (
-    //     !authStore.isAuthenticated &&
-    //     !noAuthRoutes.includes(toPath) &&
-    //     toPath !== "/login"
-    // )
-    //     return navigateTo("/login");
-    //
-    // if (authStore.isAuthenticated && toPath === "/login")
-    //     return navigateTo("/dashboard");
+    const config = useRuntimeConfig();
+
+    // TODO: Remove condition if API is hosted
+    if (config.public.isLocalhost) {
+        const auth = useAuthStore();
+        const noAuthRoutes = ["/", "/register"];
+        const router = useRouter();
+        const validRoutes = router
+            .getRoutes()
+            .map((route) => route.path.toLowerCase());
+
+        const toPath = to.path.toLowerCase();
+
+        if (!validRoutes.includes(toPath)) return navigateTo("/dashboard");
+
+        if (
+            !auth.isAuthenticated &&
+            !noAuthRoutes.includes(toPath) &&
+            toPath !== "/login"
+        )
+            return navigateTo("/login");
+
+        if (auth.isAuthenticated && toPath === "/login")
+            return navigateTo("/dashboard");
+
+        const requiredPermission = to.meta?.permission;
+        if (requiredPermission && !auth.can(String(requiredPermission))) {
+            return navigateTo("/unauthorized");
+        }
+    }
 });
